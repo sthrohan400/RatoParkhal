a93a6da397cab4227900bd601876238c42b93b6a
RohanStha <stharohan400@gmail.com>
2017-03-19 12:20:13 +0545

    initial Design on revival

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+//scroll to top of the menu
+		$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0}, 1800);
+        return false;
+    });
+
+});
+
